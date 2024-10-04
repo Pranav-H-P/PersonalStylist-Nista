7c6bdf18c11240d9ec236205842f8e6a8060a7de
Pranav <pranavhp871@gmail.com>
2024-10-04 12:47:39 +0530

    Set up of basic html css js serving done

---
+++ main.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template, redirect, url_for, session
+
+
+
+
+app = Flask(__name__)
+
+keys = open(".keys") # reading secret key
+app.secret_key = keys.readline().strip()
+
+
+@app.route("/") # Home page
+def index():
+    return render_template("index.html")
+
+
+@app.route("/createProfile") # for setting up new profile
+def createProfile():
+    return render_template("signUp.html", mode = "creation")
+
+
+@app.route("/updateProfile") # for updating already existing profile
+def updateProfile():
+    return render_template("signUp.html", mode = "updation")
+
+
+@app.route("/chat") # LLM chat page
+def chatPage():
+    return render_template("chat.html")
+
+
+if __name__ == "__main__":
+    app.run(debug = True, host = "0.0.0.0", port= 4321)
