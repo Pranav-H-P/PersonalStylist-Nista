858b650904e01e7fad08607f4c168b84028fea8f
Pranav <pranavhp871@gmail.com>
2024-10-06 11:06:35 +0530

    added text entry and button and stuff, its responsive too. Next I should add the chat bubbles and stuff

--- static/scripts/chat.js
+++ static/scripts/chat.js
@@ -2,10 +2,31 @@ const textBox = document.getElementById("textInput");
 const sendButton = document.getElementById("sendButton");
 
 const logoText = document.getElementById('logoText');
+const content = document.getElementById("mainContent");
 
 
+setTimeout( () => { // wait for a bit for it to load
+    content.classList.remove('comeFromBottom'); // trigger css transition effect
+    
+}, 250);
 
 
 logoText.addEventListener('click', () => {
     window.location.href = "/";
+});
+
+textBox.addEventListener('keydown', e => {
+    
+    if (e.key === 'Enter') {
+      
+      e.preventDefault(); // Prevent the default behavior of the Enter key (next line)
+      
+      sendButton.click();
+    }
+});
+  
+sendButton.addEventListener('click', () => {
+
+    console.log("sending to server");
+
 });
